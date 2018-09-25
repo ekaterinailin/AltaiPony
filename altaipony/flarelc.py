f84b2e5ab16dfa8e7553a0821e7009de37208873
gully <igully@gmail.com>
2018-09-25 15:55:31 -0700

    Tweaks to get the inheritance for FlareLightCurve to work

--- altaipony/flarelc.py
+++ altaipony/flarelc.py
@@ -4,14 +4,19 @@ class FlareLightCurve(KeplerLightCurve):
     """
     Flare light curve
     """
-    def __init__(self, time=None, flux=None, flux_err=None, time_format=None,
-                 time_scale=None, targetid=None, label=None, meta={},
+    def __init__(self, time=None, flux=None, flux_err=None, time_format=None, time_scale=None,
+                 centroid_col=None, centroid_row=None, quality=None, quality_bitmask=None,
+                 channel=None, campaign=None, quarter=None, mission=None, cadenceno=None,
+                 targetid=None, ra=None, dec=None, label=None, meta={},
                  breakpoints=None, flares=None):
 
-        super(KeplerLightCurve, self).__init__(self, time=None, flux=None,
-                                               flux_err=None, time_format=None,
-                                               time_scale=None, targetid=None,
-                                               label=None, meta={})
-        self.breakpoints =
-        self.flares = pd.DataFrame(columns=['istart','istop','cstart','cstop',
-                                             'ed'])
+        super(FlareLightCurve, self).__init__(time=time, flux=flux, flux_err=flux_err, time_format=time_format, time_scale=time_scale,
+                                              centroid_col=centroid_col, centroid_row=centroid_row, quality=quality,
+                                              quality_bitmask=quality_bitmask, channel=channel, campaign=campaign, quarter=quarter,
+                                              mission=mission, cadenceno=cadenceno, targetid=targetid, ra=ra, dec=dec, label=label,
+                                              meta=meta)
+        self.breakpoints = breakpoints
+        self.flares = flares #pd.DataFrame(columns=['istart','istop','cstart','cstop', 'ed'])
+
+    def __repr__(self):
+        return('FlaresLightCurve(ID: {})'.format(self.targetid))
